97f493985d77740a559a7e7adfac99d3e58fb803
macbook <shayan.noje@gmail.com>
2020-05-26 02:01:06 +0430

    fr

--- bigbluebutton-html5/imports/ui/components/nav-bar/settings-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/settings-dropdown/component.jsx
@@ -229,7 +229,11 @@ class SettingsDropdown extends PureComponent {
 
     return _.compact([
 
-
+      (<RecordingIndicator
+          key="list-item-record"
+          mountModal={mountModal}
+          amIModerator={amIModerator}
+      />),
       (<DropdownListSeparator key={_.uniqueId('list-separator-')} />),
       this.getFullscreenItem(),
       (<DropdownListItem
