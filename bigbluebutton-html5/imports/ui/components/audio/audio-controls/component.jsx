1bab58b08a9e0f2d5ef69317e1e58e36c150b9d3
macbook <shayan.noje@gmail.com>
2020-05-25 16:07:30 +0430

    fr

--- bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
@@ -90,7 +90,7 @@ class AudioControls extends PureComponent {
               color={!muted ? 'primary' : 'default'}
               ghost={muted}
               // icon={muted ? 'mute' : 'unmute'}
-              customIcon={muted? <i className="fas fa-volume-mute"></i>:<i className="fas fa-headphones-alt"></i>}
+              customIcon={muted? <i className="fas fa-headset"></i>: <i className="fas fa-microphone-alt-slash"></i>}
               size="lg"
               circle
               accessKey={shortcuts.toggleMute}
