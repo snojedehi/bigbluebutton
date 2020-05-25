c208f221d7db5970017c9b80ce7cea3301c1f93b
macbook <shayan.noje@gmail.com>
2020-05-25 15:37:43 +0430

    fr

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
@@ -211,7 +211,7 @@ class ActionsDropdown extends PureComponent {
             aria-label={intl.formatMessage(intlMessages.actionsLabel)}
             className={styles.button}
             label={intl.formatMessage(intlMessages.actionsLabel)}
-            customIcon={<i className="far fa-file-powerpoint"></i>}
+            customIcon={<i className="fas fa-archive"></i>}
             color="primary"
             size="lg"
             circle
