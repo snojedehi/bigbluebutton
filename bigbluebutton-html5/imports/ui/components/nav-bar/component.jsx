08ce593c0a1981c5fb6e3353f6925dcb52efecdb
snojedehi <snojedehi@vandud.com>
2020-05-26 17:00:44 +0430

    daf

--- bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
@@ -104,19 +104,6 @@ class NavBar extends PureComponent {
               aria-expanded={isExpanded}
               accessKey={TOGGLE_USERLIST_AK}
             />
-              <Button
-                  data-test="chatListToggleButton"
-                  // onClick={NavBar.handleToggleUserList}
-                  ghost
-                  circle
-                  hideLabel
-                  label={intl.formatMessage(intlMessages.toggleUserListLabel)}
-                  aria-label={ariaLabel}
-                  icon="user"
-                  className={cx(toggleBtnClasses)}
-                  aria-expanded={isExpanded}
-                  accessKey={TOGGLE_USERLIST_AK}
-              />
           </div>
 
           <div className={styles.center}>
