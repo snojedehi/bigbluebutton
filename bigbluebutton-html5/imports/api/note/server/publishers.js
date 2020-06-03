8b0010561318da80937bee3404a6a466c27ed8e0
macbook <shayan.noje@gmail.com>
2020-06-03 22:58:41 +0430

    fixup! Revert "fr"

--- bigbluebutton-html5/imports/api/note/server/publishers.js
+++ bigbluebutton-html5/imports/api/note/server/publishers.js
@@ -4,7 +4,7 @@ import Note from '/imports/api/note';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 
 function note() {
-  console.log("kkkkkkkkk")
+  console.log("kkkkkkkkk2")
   if (!this.userId) {
     return Note.find({ meetingId: '' });
   }
