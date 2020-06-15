b17c153bc680330696041023c7537f4c5305403d
macbook <shayan.noje@gmail.com>
2020-06-16 01:09:15 +0430

    fixup! Revert "fr"

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
@@ -217,7 +217,7 @@ class ActionsDropdown extends PureComponent {
             onClick={() => null}
           />
         </DropdownTrigger>
-        <DropdownContent placement="bottom right">
+        <DropdownContent placement="bottom left">
           <DropdownList>
             {availableActions}
           </DropdownList>
