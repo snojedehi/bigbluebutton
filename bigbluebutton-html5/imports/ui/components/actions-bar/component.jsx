d2e7933909cfd71a9361777987dd91be18ee92a1
macbook <shayan.noje@gmail.com>
2020-06-16 01:14:05 +0430

    fixup! Revert "fr"

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -8,6 +8,7 @@ import AudioControlsContainer from '../audio/audio-controls/container';
 import JoinVideoOptionsContainer from '../video-provider/video-button/container';
 import CaptionsButtonContainer from '/imports/ui/components/actions-bar/captions/container';
 import PresentationOptionsContainer from './presentation-options/component';
+import SettingsDropdownContainer from "../nav-bar/settings-dropdown/container";
 
 class ActionsBar extends PureComponent {
   render() {
@@ -47,6 +48,9 @@ class ActionsBar extends PureComponent {
     return (
       <div className={styles.actionsbar}>
         <div className={styles.left}>
+          <div className={styles.right}>
+            <SettingsDropdownContainer amIModerator={amIModerator} mountModal={mountModal} />
+          </div>
           <ActionsDropdown {...{
             amIPresenter,
             amIModerator,
