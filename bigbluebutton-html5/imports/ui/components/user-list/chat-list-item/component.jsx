c61ec74ce8e3ae985a70f45a610f7476c5c87b3a
macbook <shayan.noje@gmail.com>
2020-06-18 15:52:17 +0430

    fixup! Revert "fr"

--- bigbluebutton-html5/imports/ui/components/user-list/chat-list-item/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/chat-list-item/component.jsx
@@ -78,7 +78,7 @@ const ChatListItem = (props) => {
     <div
       data-test="chatButton"
       role="button"
-      className={cx(styles.chatListItem, linkClasses)}
+      className={cx(styles.chatListItem, linkClasses,isPublicChat(chat) ? "publicIcon" : "")}
       aria-expanded={isCurrentChat}
       tabIndex={tabIndex}
       accessKey={isPublicChat(chat) ? TOGGLE_CHAT_PUB_AK : null}
