c4111b0c5f00db2196509617b16642bba25dbe8e
macbook <shayan.noje@gmail.com>
2020-06-19 04:15:49 +0430

    fixup! Revert "fr"

--- bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
@@ -295,7 +295,7 @@ class MessageForm extends PureComponent {
             onChange={this.handleMessageChange}
             onKeyDown={this.handleMessageKeyDown}
           />
-          <div className={styles.emojiHolder}  style={emoji?"":"display:none"}>
+          <div className={styles.emojiHolder}  >
             sas
           </div>
           <Button
