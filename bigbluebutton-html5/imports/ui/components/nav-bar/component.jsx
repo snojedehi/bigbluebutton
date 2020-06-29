3ad148b3c46bd9fa42d60f4d46ad7967d216e1cb
snojedehi <snojedehi@vandud.com>
2020-06-29 15:53:20 +0430

    daf

--- bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
@@ -79,7 +79,7 @@ class NavBar extends PureComponent {
       amIModerator,
       meetingInfo,
     } = this.props;
-    console.log("meetingInfo",meetingInfo)
+    console.log("meetingInfo",meetingInfo())
 
     const toggleBtnClasses = {};
     toggleBtnClasses[styles.btn] = true;
